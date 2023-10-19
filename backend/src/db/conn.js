e8ab63c2ac703c6ba48495c6a30f34e161ac170c
Sultanomar0013 <sultanomar756@gmail.com>
2023-10-19 10:11:06 +0600

    db connection with atlas

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,10 +1,12 @@
-const mongoose = require("mongoose");
+const app = require('express')();
+const http = require('http').Server(app);
 
-mongoose.connect("mongodb://127.0.0.1:27017/travelmanagement", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://sultanomar:1234@travel.j6o0tig.mongodb.net/TravelManagement?retryWrites=true&w=majority")
+.then(() => {
     console.log(`Connection successful`);
 }).catch((e) => {
     console.log(`Connection error: ${e}`);
 });
+const User = require('../models/signup')
